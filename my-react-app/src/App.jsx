bfa4d76b98a7a6eae3d98e3d61bced90dbbb91a1
Virendra Sahu <VIRENDRA9334>
2025-07-27 12:21:20 +0530

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Stopwatch from "./StopWatch.jsx";
+
+function App() {
+  return (
+    <>
+      <Stopwatch />
+      <Stopwatch />
+      <Stopwatch />
+    </>
+  );
+}
+
+export default App;
